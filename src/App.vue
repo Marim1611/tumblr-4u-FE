f672799a4730654b0943acd1b30783df75f7ada0
nour-aldin <nourdx73@gmail.com>
2021-11-12 22:09:42 +0200

    First page with the Signin button added

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
   <div id="app">
+     <Header></Header>
+      <component v-bind:is="component"></component>
+      <button v-on:click="component='signIn'">CLICKMEE!!!!</button>
+        
+        
+     
+
      
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Content_1 from './components/Content_1.vue'
+import SignIn from './components/SignIn.vue'
+
+
+
  
 
 export default {
+
   name: 'App',
+
   components: {
+
+    Header,
+    'signIn' : SignIn,
+    'content_1': Content_1
+    
+    
      
+  },
+  data(){
+
+    return{
+
+      component: 'content_1'
+    }
+    
   }
 }
+
+
 </script>
 
 <style>
- 
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
 </style>
