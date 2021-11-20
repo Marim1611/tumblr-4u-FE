83338dd1dd9c537c66b1d8eb48ab96cfe0cf8648
Marim1611 <marimnaser813@gmail.com>
2021-11-20 19:48:40 +0200

    test comment

--- tests/unit/SignIn.spec.js
+++ tests/unit/SignIn.spec.js
@@ -1,10 +1,10 @@
-import { mount } from "@vue/test-utils";
-import SignIn from "../../src/components/registerPages/SignIn.vue"
-it("Call Validation metho", async () => {
+// import { mount } from "@vue/test-utils";
+// import SignIn from "../../src/components/registerPages/SignIn.vue"
+// it("Call Validation metho", async () => {
 
-   const wrapper = mount(SignIn, {
-    Data: {
-      name: "Learn something new",
-    },
-  });
-});
+//    const wrapper = mount(SignIn, {
+//     Data: {
+//       name: "Learn something new",
+//     },
+//   });
+// });
