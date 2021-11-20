b44bedd0f397dcedcf836c6955fd9ce579cb7ce8
Esraa-Amr <esraaamr00@hotmail.com>
2021-11-20 23:48:29 +0200

    viewPost

--- tests/unit/showPost.spec.js
+++ tests/unit/showPost.spec.js
@@ -1,41 +1,34 @@
-import { mount } from '@vue/test-utils';
-import showPost from '@/components/search.vue';
+import Vuex from 'vuex';
+import { mount,createLocalVue } from '@vue/test-utils';
+import showPost from '@/components/homePage/HomePageViewPost.vue';
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 describe('showPost unit tests: ', () =>{
     test('calls postOption when the ... is clicked', () =>{
-        const wrapper = mount(showPost, {
-            data:{
-                blogs: {
-                    id:['cataster','sillydegu','iamstrongallonmyown'],
-                    photos:['https://64.media.tumblr.com/eb7f04ce18aa06c84b653829eea3a252/3e53fe6da9921150-fb/s640x960/7e1cb8b752e64ab2180513c5f01b92211d72a77d.jpg'],
-                    content:['It\'s his egg now','Source: reddit.com','<img width="100%" src="https://64.media.tumblr.com/eb7f04ce18aa06c84b653829eea3a252/3e53fe6da9921150-fb/s640x960/7e1cb8b752e64ab2180513c5f01b92211d72a77d.jpg" alt=""><strong>Every morning, we get a chance to be different.</strong> A chance to change. A chance to be better. Your past is your past. Leave it there. Get on with the future part. Nicole Williams'],
-                    tags:['#Nicole Williams','#relationship','#love','#breakup','#heartbreak','#moving on','#strength','#recovery','#healing','#self compassion','#self respect','#personal development','#personal growth','#growth mindset','#mindfulness','#live in the now','#live in the present','#change','#breaking patterns','#second chance','#new chance','#new day','#let go of the past','#it’s not too late','#leave the past in the past','#quotes'],
-                    notes:['2,962','5,138','1,439']
-                    },
-                    showDropDown: false
+        const store = new Vuex.Store({
+            state: {
+            homeThemeIndex:0,
+            homeTheme: {backgroundColor: '#001935',fontColor:'white', cardColor:'#4080bf', fontStyle: 'Helvetica'},
             }
-        })
-        const showPost = jest.fn();
+        });
+        const wrapper = mount(showPost, {localVue,store});
+        const postOption = jest.fn();
         wrapper.setMethods({
-            showPost:showPost
+            postOption: postOption
         });
         wrapper.find('.options').trigger('click');
-        expect(showPost).toHaveBeenCalled();
+        expect(postOption).toHaveBeenCalled();
     });
     test('calls closeDropDown when the close button is clicked', () =>{
         const wrapper = mount(showPost, {
             data:{
-                blogs: {
-                    id:['cataster','sillydegu','iamstrongallonmyown'],
-                    photos:['https://64.media.tumblr.com/eb7f04ce18aa06c84b653829eea3a252/3e53fe6da9921150-fb/s640x960/7e1cb8b752e64ab2180513c5f01b92211d72a77d.jpg'],
-                    content:['It\'s his egg now','Source: reddit.com','<img width="100%" src="https://64.media.tumblr.com/eb7f04ce18aa06c84b653829eea3a252/3e53fe6da9921150-fb/s640x960/7e1cb8b752e64ab2180513c5f01b92211d72a77d.jpg" alt=""><strong>Every morning, we get a chance to be different.</strong> A chance to change. A chance to be better. Your past is your past. Leave it there. Get on with the future part. Nicole Williams'],
-                    tags:['#Nicole Williams','#relationship','#love','#breakup','#heartbreak','#moving on','#strength','#recovery','#healing','#self compassion','#self respect','#personal development','#personal growth','#growth mindset','#mindfulness','#live in the now','#live in the present','#change','#breaking patterns','#second chance','#new chance','#new day','#let go of the past','#it’s not too late','#leave the past in the past','#quotes'],
-                    notes:['2,962','5,138','1,439']
-                    },
-                    showDropDown: false
+                homeTheme:{backgroundColor: '#001935',fontColor:'white', cardColor:'#4080bf', fontStyle: 'Helvetica'},
+                homeThemeIndex:0,
+                showDropDown: false
             }
-        })
-        const showPost = jest.fn();
+        });
+        const closeDropDown = jest.fn();
         wrapper.setMethods({
             closeDropDown:closeDropDown
         });
