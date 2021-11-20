c42ef4bbe3e8090c909e0724d8d26997b94b4b71
nour-aldin <nourdx73@gmail.com>
2021-11-20 23:50:42 +0200

    UnitTesting Nour

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -15,6 +15,45 @@ describe('SignIn.vue', () => {
     });
     wrapper.setData({ userEmail: 'asdsadsa@gmail.g' })
     
-    expect(userEmail).toMatch(msg)
+    expect(this.userEmail).toBe('asdsadsa@gmail.g')
   })
 })
+
+describe('SignIn.vue', () => {
+  it('See if fucntion clicked', () => {
+
+    const wrapper = shallowMount(SignIn, {
+      data(){
+        return{
+        userEmail: '',
+        userPassword: '',
+      }
+    }
+        
+    });
+    wrapper.setData({ userEmail: '' })
+    
+    expect(this.userEmail).toBe('')
+  })
+})
+
+it("calls the onHabitDone method", async () => {
+  const wrapper = mount(SignIn, {
+    data() {
+      return {
+        userEmail: '',
+        userPassword: '',
+
+      }
+      
+    }
+  });
+  wrapper.setMethods({
+    Validation: jest.fn(),
+  });
+  const submit = wrapper.find("buttonTop")
+  await submit.triger("click")
+
+  
+  expect(wrapper.vm.Validation).toHaveBeenCalled();
+});
