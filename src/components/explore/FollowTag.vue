cb44480548df8716475df8228b5bfef0a7a5ea14
Marim1611 <marimnaser813@gmail.com>
2021-12-16 12:59:42 +0200

    explore partly

---
+++ src/components/explore/FollowTag.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="followTags" v-bind:style="{'background-color': homeTheme[homeThemeIndex].cardColor}" >
+        <div class="container">
+             <h5 v-bind:style="{'color': homeTheme[homeThemeIndex].fontColor, 'font-family':homeTheme[homeThemeIndex].fontStyle }">Let's get personal</h5>
+             <p v-bind:style="{'color': homeTheme[homeThemeIndex].fontColor, 'font-family':homeTheme[homeThemeIndex].fontStyle }">Whatever you’re into, Tumblr has it. Follow tags that matter to you, and the things you love will show up here.</p>
+             <button class="firButton " v-bind:style="{'background-color':homeTheme[homeThemeIndex].focused,'color': homeTheme[homeThemeIndex].backgroundColor, 'font-family':homeTheme[homeThemeIndex].fontStyle }">Let's follow some tags</button>
+             <button class="secButton" v-bind:style="{'border-color':homeTheme[homeThemeIndex].fontColor,'color': homeTheme[homeThemeIndex].fontColor, 'font-family':homeTheme[homeThemeIndex].fontStyle }">Maybe later</button>
+        </div>
+    </div>       
+</template>
+
+<script>
+ 
+ 
+ 
+export default {
+  name: "FollowTags",
+  components: {
+   
+  },
+  methods:{
+   
+  },
+  data: function () {
+    return {
+    }
+  },
+  
+  computed: {
+     
+    homeTheme: function () {
+      return this.$store.state.homeTheme;
+    },
+     
+    homeThemeIndex: function () {
+      return this.$store.state.homeThemeIndex;
+    },
+  },
+ 
+  
+};
+</script>
+
+<style scoped>
+ #followTags{
+     max-width: 350px;
+    
+     margin-top:20px;
+ }
+ .container{
+     padding: 20px;
+ }
+ .firButton{
+     border-radius: 3px;
+     text-align: center;
+     padding: 10px;
+     margin-bottom: 7px;
+     width: 100%;
+ }
+ .secButton{
+     border-radius: 3px;
+     border: solid 2px ;
+     text-align: center;
+     padding: 10px;
+     width: 100%;
+ }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 //import { createRouter, createWebHistory } from 'vue-router'
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Explore from '../components/HelloWorld.vue' 
+import Explore from '../components/explore/Explore'
 import Home from '../components/homePage/HomePage.vue'
 import SignIn from '../components/registerPages/SignIn.vue'
 import SignUp from '../components/registerPages/SignUp.vue'
