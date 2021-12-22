d00e8b85b652b422bdbe78063e079e00a72e23c3
Esraa-Amr <esraaamr00@hotmail.com>
2021-12-22 08:49:53 +0200

    mock

--- src/mocks/db.js
+++ src/mocks/db.js
@@ -1,10 +1,10 @@
 const autoCompleteSearchDash = require("./data/searchResults.json");
 const profile = require("./data/db.json");
 const posts= require("./data/posts.json");
-const getDashBoard= require("./data/dashBoard.json");
+const dashBoard= require("./data/dashBoard.json");
 module.exports = () => ({
   profile,    
   autoCompleteSearchDash,
   posts,
-  getDashBoard
+  dashBoard
 });
