d7711ed22265eddb45e2e7c64578a009ad680ec0
mariemzayn18 <mariamzayn18@gmail.com>
2021-12-30 08:33:33 +0200

    yarab

--- src/components/createPost/editors/imageModal.vue
+++ src/components/createPost/editors/imageModal.vue
@@ -138,6 +138,7 @@ export default {
             }
           )
           .then((res) => {
+            console.log(res.data);
             this.imagesURLS = res.data.file;
             console.log("images url weslo elhamdullahh");
             console.log(res.data.file);
